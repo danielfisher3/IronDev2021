13e9c50b0d3868ea36250a5e3b938da8a092c082
danielfisher3 <38573896+danielfisher3@users.noreply.github.com>
2021-11-22 11:26:18 -0600

    git attributes updated
    updated the attributes

--- IronDev2021/Assets/Daniel/Scripts/HandController.cs
+++ IronDev2021/Assets/Daniel/Scripts/HandController.cs
@@ -1,23 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.XR.Interaction.Toolkit;
-
-[RequireComponent(typeof(ActionBasedController))]
-public class HandController : MonoBehaviour
-{
-    ActionBasedController controller;
-    public Hand hand;
-    // Start is called before the first frame update
-    void Start()
-    {
-        controller = GetComponent<ActionBasedController>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        hand.SetGrip(controller.selectAction.action.ReadValue<float>());
-        hand.SetTrigger(controller.activateAction.action.ReadValue<float>());
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:1bcc80585a6258983a4a7e54872865a1ae11c4b26676e38e8b12be392f74035a
+size 666
--- IronDev2021/Assets/Unity_Default_Project_ Assets/TutorialInfo/Scripts/Readme.cs
+++ IronDev2021/Assets/Unity_Default_Project_ Assets/TutorialInfo/Scripts/Readme.cs
@@ -1,16 +1,3 @@
-using System;
-using UnityEngine;
-
-public class Readme : ScriptableObject
-{
-    public Texture2D icon;
-    public string title;
-    public Section[] sections;
-    public bool loadedLayout;
-
-    [Serializable]
-    public class Section
-    {
-        public string heading, text, linkText, url;
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:c4cb518bc80380918d8a9d4fd497b78f5e7907b987f03396f23897a6de5f4337
+size 315
