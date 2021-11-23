e5f1906a49eba330ad0bfa0760a3f9459397be0d
danielfisher <38573896+danielfisher3@users.noreply.github.com>
2021-11-22 19:49:50 -0600

    Patrols & character standby

--- IronDev2021/Assets/Daniel/Scripts/NPC_AI/NPCAI.cs
+++ IronDev2021/Assets/Daniel/Scripts/NPC_AI/NPCAI.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5fd0572f4b24a21e61e430dee139a4553f9032a889520fc065c753a368ece104
-size 1384
+oid sha256:8719b2beb98db27e5e1042aeabb89ebb7885aba24b5da561e4e86e6189c93cec
+size 1311
